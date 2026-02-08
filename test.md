bf814f987945cea460b95f0658ceef078ab9e871
athul-ramesh <athulinc@gmail.com>
2026-02-08 09:37:58 +0530

    js file from local

--- test.md
+++ test.md
@@ -1,2 +1,3 @@
 change from local
 changes from remote
+change from local again
---
+++ tst.js
@@ -0,0 +1,8 @@
+function add(a, b) {
+  const paramA = a;
+  const paramB = b;
+  return paramA + paramB;
+}
+
+
+console.log(add(1, 2))
